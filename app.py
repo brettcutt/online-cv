e9e4f704475617d483d7f6841c75ac707646e511
brett <brettcutt@outlook.com.au>
2018-12-17 13:39:40 +1030

    Changed over to using flask

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+import os
+import json
+import env
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+
+with open("data/skills.json", 'r') as file:
+    skills = json.load(file)
+
+with open("data/history.json", 'r', encoding='utf8') as file:
+    history = json.load(file)
+
+with open("data/projects.json", 'r') as file:
+    projects = json.load(file)
+
+@app.route('/')
+def index():
+    return render_template('index.html', skills=skills,
+                                         history=history,
+                                         projects=projects)
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
