20bf3df14f5dabb788a45f2cc59664a4c03c308e
brett <brettcutt@outlook.com.au>
2018-12-17 18:35:08 +1030

    added environment variables for port and host

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 import os
 import json
-import env
+#import env
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
@@ -25,4 +25,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
