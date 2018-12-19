252a226854c66bfb223871c09ea360ca21810963
brett <brettcutt@outlook.com.au>
2018-12-19 11:19:16 +1030

    added mav menu animation

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -1,12 +1,44 @@
 $(document).ready(function () {
-
+    // Burger menu spinning action
     $('#menu-bars').click(function () {
-        $("#collapse").toggle("blind", 1000);
-    })
+        if ($(".bar1").hasClass('forwards')) {
 
+            $("#collapse").show("blind", 1000);
+            $('.bar1').removeClass('bar1-animate-r')
+            $('.bar2').removeClass('bar2-animate-r')
+            $('.bar3').removeClass('bar3-animate-r')
+            $('.bar1').addClass('bar1-animate')
+            $('.bar2').addClass('bar2-animate')
+            $('.bar3').addClass('bar3-animate')
+            $('#menu-bars').children().removeClass('forwards')
+            $('#menu-bars').children().addClass('backwards')
+        } else {
+            $("#collapse").hide("blind", 1000);
+            $('.bar1').removeClass('bar1-animate')
+            $('.bar2').removeClass('bar2-animate')
+            $('.bar3').removeClass('bar3-animate')
+            $('.bar1').addClass('bar1-animate-r')
+            $('.bar2').addClass('bar2-animate-r')
+            $('.bar3').addClass('bar3-animate-r')
+            $('#menu-bars').children().addClass('forwards')
+            $('#menu-bars').children().removeClass('backwards')
+        }
+    })
+    // Burger menu spinning action
+
+    // blind down navmenu
     $(".mobile-nav li").click(function () {
         $("#collapse").toggle("blind", 1000);
+        $('.bar1').removeClass('bar1-animate')
+        $('.bar2').removeClass('bar2-animate')
+        $('.bar3').removeClass('bar3-animate')
+        $('.bar1').addClass('bar1-animate-r')
+        $('.bar2').addClass('bar2-animate-r')
+        $('.bar3').addClass('bar3-animate-r')
+        $('#menu-bars').children().addClass('forwards')
+        $('#menu-bars').children().removeClass('backwards')
     })
+    // blind down navmenu
 
     // NAVBAR ANIMATION 
 
